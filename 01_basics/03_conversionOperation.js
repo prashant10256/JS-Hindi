2542fdf2663b5ade1b3186624db901e5a93d9e92
Prashant_Sugave <95568247+prashant10256@users.noreply.github.com>
2023-09-26 04:05:12 +0000

    Conversion of dattypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33abc";
+
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33"=> 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booeanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booeanIsLoggedIn);
+
+// 1 => true; 0 => false
+// " " => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
