6c2ed7bf9d4ca8fcf3f6d86cca32ef71ba7b3a93
Prashant_Sugave <95568247+prashant10256@users.noreply.github.com>
2023-09-30 07:52:28 +0000

    conversion dattypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -15,7 +15,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = "hitesh"
 
 let booeanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booeanIsLoggedIn);
+// console.log(booeanIsLoggedIn);
 
 // 1 => true; 0 => false
 // " " => false
@@ -24,4 +24,45 @@ console.log(booeanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+//**************************** operations ************************************* 
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "hello";
+let str2 = " hitesh";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
