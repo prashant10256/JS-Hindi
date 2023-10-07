33f0278654f316c20362d65087ca79163e53f6a5
Prashant_Sugave <95568247+prashant10256@users.noreply.github.com>
2023-10-07 02:55:57 +0000

    Control Flow

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,5 +1,5 @@
 // if
-const isUserloggedIn = true
+// const isUserloggedIn = true
 const temperature = 41;
 
 // if(temperature === 40){
@@ -21,4 +21,32 @@ const temperature = 41;
 // console.log(`User power: ${power}`);
 
 
+// const balence = 1000
 
+// if (balence > 500) console.log("test"),console.log("test2");
+
+// if(balence < 500){
+//     console.log("less than 500");
+// }else if(balence < 750){
+//     console.log("less than 750");
+
+// }else if(balence < 900) {
+//     console.log("less than 900");
+
+// }else{
+//     console.log("less than 1200");
+// }
+
+
+const isUserloggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(isUserloggedIn && debitCard  && 2==3){
+     console.log("Allow to buy courses");
+}
+
+if(loggedInFromGoogle || loggedInFromEmail || guestUser){
+    console.log("User logged in");
+}
